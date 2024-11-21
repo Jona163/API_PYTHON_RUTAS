b4db1653c3657d887604334008c846223a87f16d
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-21 10:33:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 #Consumo de API de google Maps en Python
 import requests
 import folium
+
+def obtener_ruta(partida, destino, paradas, api_key):
+    origin = partida.replace(" ", "+")
+    destination = destino.replace(" ", "+")
+    waypoints = "|".join(paradas.split(","))
+  
