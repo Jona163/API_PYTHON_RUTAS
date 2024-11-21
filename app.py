030f9ac62bc83dd9f9858d6f2adc1bf816a2d34a
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-11-21 10:33:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,5 @@ def obtener_ruta(partida, destino, paradas, api_key):
     destination = destino.replace(" ", "+")
     waypoints = "|".join(paradas.split(","))
   
+   #Acceso a google Maps
+    url = f"https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&waypoints={waypoints}&key={api_key}"
